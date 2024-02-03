d59316dd1fc272f3b58f03945ff91f27c0365406
Василий Логинов <97108615+LVVS666@users.noreply.github.com>
2024-02-03 11:37:40 +0700

    actions_github_ci

--- page/login.py
+++ page/login.py
@@ -6,8 +6,10 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-test_email = os.getenv('email')
-test_password = os.getenv('password')
+test_email = 'pupok777vasya@gmail.com'
+test_password = 'Password1!'
+# test_email = os.getenv('email')
+# test_password = os.getenv('password')
 email_form = (By.XPATH, '//input[@placeholder="Введите email"]')
 password_form = (By.XPATH, '//input[@placeholder="Введите пароль"]')
 button_class = (By.CLASS_NAME, 'MuiButton-containedPrimary')
