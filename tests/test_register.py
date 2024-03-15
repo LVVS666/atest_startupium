caa9530017e76e33fc3d34b85e622b4917c0ce42
Василий Логинов <97108615+LVVS666@users.noreply.github.com>
2024-03-15 11:39:36 +0700

    all_allure_report_complete

--- tests/test_register.py
+++ tests/test_register.py
@@ -132,187 +132,245 @@ def test_all_form_not_name(browser):
         assert message_error is not None, 'Сообщение о незаполненном поле имени не появилось'
 
 
+@allure.feature('Регистрация')
+@allure.story('Невалидные данные и отработка ошибок')
+@allure.title('Уведомление,что в поле имени меньше трех букв')
 def test_name_two_leng(browser):
-    '''Проверка поля имени меньше трех букв'''
     register_page = Register(browser)
     register_page.open()
     register_page.wait_element(name_form_path)
     element_email = register_page.find(name_form_path)
-    element_email.send_keys('QA')
+    with allure.step('Заполнить поле имени двумя любыми буквами'):
+        element_email.send_keys('QA')
     register_page.wait_element(warning_form_not_leng_name)
-    message_error = register_page.find(warning_form_not_leng_name)
+    with allure.step('Сообщение о невалидности поле имени'):
+        message_error = register_page.find(warning_form_not_leng_name)
     assert message_error is not None, 'Сообщение не менее трех букв в имени не появилось'
 
 
+@allure.feature('Регистрация')
+@allure.story('Валидные данные')
+@allure.title('Поле имени заполнено из трех букв')
 def test_name_three_leng(browser):
-    '''Проверка поля имени из трех букв'''
     register_page = Register(browser)
     register_page.open()
     register_page.wait_element(name_form_path)
     element_email = register_page.find(name_form_path)
-    element_email.send_keys('AQA')
+    with allure.step('Заполнить поле имени тремя любыми буквами'):
+        element_email.send_keys('AQA')
     try:
-        message = register_page.find(warning_form_not_leng_name)
+        with allure.step('Уведомление об ошибке отсутствует'):
+            message = register_page.find(warning_form_not_leng_name)
         assert message is None, 'Сообщение менее трех букв появилось'
     except NoSuchElementException:
         f'Сообщение менее трех букв в имени не появилось'
 
 
+@allure.feature('Регистрация')
+@allure.story('Невалидные данные и отработка ошибок')
+@allure.title('Незаполненно поле email')
 def test_all_form_not_email(browser):
-    '''Проверка незаполненного поля email'''
     register_page = Register(browser)
     register_page.open()
     register_page.wait_element(name_form_path)
     element_name = register_page.find(name_form_path)
-    element_name.send_keys(test_name)
+    with allure.step('Поле имени заполнено валидными данными'):
+        element_name.send_keys(test_name)
     register_page.wait_element(password_form_path)
     password_element = register_page.find(password_form_path)
-    password_element.send_keys(test_password)
+    with allure.step('Поле пароля заполнено валидными данными'):
+        password_element.send_keys(test_password)
     register_page.wait_element(password_repeat_path)
     password_rep_element = register_page.find(password_repeat_path)
-    password_rep_element.send_keys(test_password)
+    with allure.step('Поле повторного пароля заполнено валидными данными'):
+        password_rep_element.send_keys(test_password)
     button = register_page.find(button_class)
     button.click()
     register_page.wait_element(warning_form_path)
-    message_error = register_page.find(warning_form_path)
+    with allure.step('Появилось сообщение о незаполненном поле email'):
+        message_error = register_page.find(warning_form_path)
     assert message_error is not None, 'Сообщение о незаполненном поле email не появилось'
 
 
+@allure.feature('Регистрация')
+@allure.story('Невалидные данные и отработка ошибок')
+@allure.title('Незаполненно поле пароля')
 def test_all_form_not_password(browser):
-    '''Проверка незаполненного поля пароля'''
     register_page = Register(browser)
     register_page.open()
     register_page.wait_element(name_form_path)
     element_name = register_page.find(name_form_path)
-    element_name.send_keys(test_name)
+    with allure.step('Поле имени заполнено валидными данными'):
+        element_name.send_keys(test_name)
     register_page.wait_element(email_form_path)
     email_element = register_page.find(email_form_path)
-    email_element.send_keys(test_email)
+    with allure.step('Поле email заполнено валидными данными'):
+        email_element.send_keys(test_email)
     button = register_page.find(button_class)
     button.click()
     register_page.wait_element(warning_form_path)
-    message_error = register_page.find(warning_form_path)
+    with allure.step('Появилось уведомление о незаполненном поле пароля'):
+        message_error = register_page.find(warning_form_path)
     assert message_error is not None, 'Сообщение о незаполненном поле пароля не появилось'
 
 
+@allure.feature('Регистрация')
+@allure.story('Невалидные данные и отработка ошибок')
+@allure.title('Незаполненно поле повторного пароля')
 def test_all_form_not_password_repeat(browser):
-    '''Проверка незаполненного поля пароля'''
     register_page = Register(browser)
     register_page.open()
     register_page.wait_element(name_form_path)
     element_name = register_page.find(name_form_path)
-    element_name.send_keys(test_name)
+    with allure.step('Поле имени заполнено валидными данными'):
+        element_name.send_keys(test_name)
     register_page.wait_element(email_form_path)
     email_element = register_page.find(email_form_path)
-    email_element.send_keys(test_email)
+    with allure.step('Поле имени заполнено валидными данными'):
+        email_element.send_keys(test_email)
     register_page.wait_element(password_form_path)
     password_element = register_page.find(password_form_path)
-    password_element.send_keys(test_password)
+    with allure.step('Поле пароля заполнено валидными данными'):
+        password_element.send_keys(test_password)
     button = register_page.find(button_class)
     button.click()
     register_page.wait_element(warning_form_path)
-    message_error = register_page.find(warning_form_path)
+    with allure.step('Появилось уведомление о незаполненном повторном поле пароля'):
+        message_error = register_page.find(warning_form_path)
     assert message_error is not None, 'Сообщение о незаполненном повторном поле пароля не появилось'
 
 
+@allure.feature('Регистрация')
+@allure.story('Невалидные данные и отработка ошибок')
+@allure.title('Отсутствие цифр в пароле')
 def test_form_password_not_number(browser):
-        '''Проверка появления уведомления об отсуствие цифр в пароле'''
         register_page = Register(browser)
         register_page.open()
         register_page.wait_element(password_form_path)
         element_password = register_page.find(password_form_path)
-        element_password.send_keys('Test_password')
+        with allure.step('Заполнить поле пароля текстом без цифр'):
+            element_password.send_keys('Test_password')
         register_page.wait_element(warning_form_password_number)
-        form_elements = register_page.find(warning_form_password_number)
+        with allure.step('Появилось уведомление об отсутствие цифр в пароле'):
+            form_elements = register_page.find(warning_form_password_number)
         assert form_elements is not None, 'Сообщение о не валидности пароля не появилось'
 
 
+@allure.feature('Регистрация')
+@allure.story('Невалидные данные и отработка ошибок')
+@allure.title('Отсутствие прописной буквы в пароле')
 def test_form_password_not_upper_leng(browser):
     '''Проверка появления уведомления об отсуствие прописной буквы в пароле'''
     register_page = Register(browser)
     register_page.open()
     register_page.wait_element(password_form_path)
     element_password = register_page.find(password_form_path)
-    element_password.send_keys('test_password1')
+    with allure.step('Заполнить поле пароля текстом с цифрой и без прописной буквы.'):
+        element_password.send_keys('test_password1')
     register_page.wait_element(warning_form_password_lenght)
-    assert register_page.find(warning_form_password_lenght), 'Сообщение о не валидности пароля не появилось'
+    with allure.step('Появилось уведомление об отсутствие прописной буквы в пароле'):
+        assert register_page.find(warning_form_password_lenght), 'Сообщение о не валидности пароля не появилось'
 
 
+@allure.feature('Регистрация')
+@allure.story('Невалидные данные и отработка ошибок')
+@allure.title('Отсутствие символа в пароле')
 def test_form_password_not_symbol(browser):
-    '''Проверка появления уведомления об отсуствие символа в пароле'''
     register_page = Register(browser)
     register_page.open()
     register_page.wait_element(password_form_path)
     element_password = register_page.find(password_form_path)
-    element_password.send_keys('Testpassword1')
+    with allure.step('Заполнить поле пароля текстом с цифрой и прописной буквы, без символа'):
+        element_password.send_keys('Testpassword1')
     register_page.wait_element(warning_form_password_sybmol)
-    assert register_page.find(warning_form_password_sybmol) is not None, 'Сообщение о не валидности пароля не появилось'
+    with allure.step('Появилось уведомление об отсутствие символа в пароле'):
+        assert register_page.find(warning_form_password_sybmol) is not None, 'Сообщение о не валидности пароля не появилось'
 
 
+@allure.feature('Регистрация')
+@allure.story('Невалидные данные и отработка ошибок')
+@allure.title('Несовпадение повторного пароля')
 def test_password_mismatch(browser):
-        '''Проверка появления уведомления об несовпадение пароля в поле повторного пароля'''
         register_page = Register(browser)
         register_page.open()
         register_page.wait_element(password_form_path)
         element_password = register_page.find(password_form_path)
-        element_password.send_keys('Test_password1')
+        with allure.step('Ввести валидный пароль в поле'):
+            element_password.send_keys('Test_password1')
         register_page.wait_element(password_repeat_path)
         element_password_rep = register_page.find(password_repeat_path)
-        element_password_rep.send_keys('Test_password2')
+        with allure.step('Ввести другой пароль в поле повторного пароля'):
+            element_password_rep.send_keys('Test_password2')
         register_page.wait_element(warning_form_not_password)
-        form_elements = register_page.find(warning_form_not_password)
+        with allure.step('Уведомление о несовпадение паролей'):
+            form_elements = register_page.find(warning_form_not_password)
         assert form_elements is not None, 'Сообщение о не совпадение паролей'
 
 
+@allure.feature('Регистрация')
+@allure.title('Успешная регистрация при валидных данных')
 def test_complete_register(browser):
-    '''Проверка успешной регистрации при валидных данных'''
     register_page = Register(browser)
     register_page.open()
     register_page.wait_element(name_form_path)
     element_name = register_page.find(name_form_path)
-    element_name.send_keys(test_name)
+    with allure.step('Поле имени заполнено валидными данными'):
+        element_name.send_keys(test_name)
     register_page.wait_element(email_form_path)
     email_element = register_page.find(email_form_path)
-    email_element.send_keys(test_email)
+    with allure.step('Поле email заполнено валидными данными'):
+        email_element.send_keys(test_email)
     register_page.wait_element(password_form_path)
     password_element = register_page.find(password_form_path)
-    password_element.send_keys(test_password)
+    with allure.step('Поле пароля заполнено валидными данными'):
+        password_element.send_keys(test_password)
     register_page.wait_element(password_repeat_path)
     password_repeat_element = register_page.find(password_repeat_path)
-    password_repeat_element.send_keys(test_password)
+    with allure.step('Поле повторного пароля заполнено валидными данными'):
+        password_repeat_element.send_keys(test_password)
     button = register_page.find(button_class)
     button.click()
     register_page.wait_element(complete_register, time=20)
-    complete_element = register_page.find(complete_register)
+    with allure.step('Уведомление об успешной регистрации'):
+        complete_element = register_page.find(complete_register)
     assert complete_element is not None, 'Сообщение об успешной регистрации не появилось'
 
 
+@allure.feature('Регистрация')
+@allure.story('Невалидные данные и отработка ошибок')
+@allure.title('Ошибка при повторной регистрации на одну почту')
 def test_repeat_user(browser):
-    '''Проверка повторной регистрации для созданного пользователя'''
-    def register_on(browser):
-        register_page = Register(browser)
-        register_page.open()
-        register_page.wait_element(name_form_path)
-        element_name = register_page.find(name_form_path)
-        element_name.send_keys(test_name)
-        register_page.wait_element(email_form_path)
-        email_element = register_page.find(email_form_path)
-        email_element.send_keys('test_email@gmail.com')
-        register_page.wait_element(password_form_path)
-        password_element = register_page.find(password_form_path)
-        password_element.send_keys(test_password)
-        register_page.wait_element(password_repeat_path)
-        password_repeat_element = register_page.find(password_repeat_path)
-        password_repeat_element.send_keys(test_password)
-        register_page.wait_element(button_class)
-        button = register_page.find(button_class)
-        button.click()
-        button.click()
-        register_page.wait_element(warning_not_register)
-        error_register_repeat = register_page.find(warning_not_register)
-        return error_register_repeat
-    repeat_register = register_on(browser)
-    assert repeat_register is not None, 'Повторная регистрация с зарегистрированным email удалась'
+    with allure.step('Регистрация первого пользователя'):
+        def register_on(browser):
+            register_page = Register(browser)
+            register_page.open()
+            register_page.wait_element(name_form_path)
+            element_name = register_page.find(name_form_path)
+            with allure.step('Поле имени заполнено валидными данными'):
+                element_name.send_keys(test_name)
+            register_page.wait_element(email_form_path)
+            email_element = register_page.find(email_form_path)
+            with allure.step('Поле email заполнено валидными данными'):
+                email_element.send_keys('test_email@gmail.com')
+            register_page.wait_element(password_form_path)
+            password_element = register_page.find(password_form_path)
+            with allure.step('Поле пароля заполнено валидными данными'):
+                password_element.send_keys(test_password)
+            register_page.wait_element(password_repeat_path)
+            password_repeat_element = register_page.find(password_repeat_path)
+            with allure.step('Поле повторного пароля заполнено валидными данными'):
+                password_repeat_element.send_keys(test_password)
+            register_page.wait_element(button_class)
+            button = register_page.find(button_class)
+            button.click()
+            button.click()
+            register_page.wait_element(warning_not_register)
+            error_register_repeat = register_page.find(warning_not_register)
+            return error_register_repeat
+    with allure.step('Повторная регистрация с этими же данными'):
+        repeat_register = register_on(browser)
+    with allure.step('Уведомление об ошибке об уже зарегистрированном пользователе на введенный email'):
+        assert repeat_register is not None, 'Повторная регистрация с зарегистрированным email удалась'
 
 
 def test_open_message_to_email(browser):
