b313e74ff5234008f6acae1d2a18937783b32b73
Василий Логинов <97108615+LVVS666@users.noreply.github.com>
2024-03-29 13:39:31 +0700

    search_projects_add_search_name

--- page/search_projects.py
+++ page/search_projects.py
@@ -1,11 +1,15 @@
 from .base_page import BasePage
 from selenium.webdriver.common.by import By
 
-name_project = (By.XPATH, '//h2[text()="TEST"]')
+name_project = (By.XPATH, '//h2[@class="MuiTypography-root MuiTypography-bodyM_600 css-w3of08" and text()="TEST"]')
+name_project_second = (By.XPATH, '//div[@class="MuiBox-root css-0"]/h2[text()="TEST"]')
 form_search = (By.XPATH, '//input[contains(@class, "MuiAutocomplete-input") and @placeholder="Искать"]')
-method_search = (By.XPATH, '//div[@role="combobox" and @class="MuiSelect-select MuiSelect-standard MuiSelect-multiple MuiInputBase-input MuiInput-input css-2z1u6n"]')
-search_tag = (By.XPATH, '//span[text()="по тегам"]')
 button_search = (By.XPATH, '//*[@id="__next"]/div[2]/main/div/div/form/div/div[2]/div[1]/div/button')
+
+method_search = (By.XPATH, '/html/body/div[1]/div[2]/main/div/div/form/div/div[3]/div[1]/div[1]/div/div/div')
+method_name = (By.XPATH, '//li[@class="MuiButtonBase-root MuiMenuItem-root MuiMenuItem-gutters MuiMenuItem-root MuiMenuItem-gutters css-1kkijwg" and contains(@data-value, "по названию")]/span[text()="по названию"]')
+form_name_search = (By.XPATH, '//div[@class="MuiInputBase-root MuiOutlinedInput-root MuiInputBase-colorPrimary MuiInputBase-fullWidth Mui-focused MuiInputBase-formControl MuiInputBase-sizeSmall MuiInputBase-adornedEnd MuiAutocomplete-inputRoot css-9xl5kx"]/input[@id=":ra:" and @class="MuiInputBase-input MuiOutlinedInput-input MuiInputBase-inputSizeSmall MuiInputBase-inputAdornedEnd MuiAutocomplete-input MuiAutocomplete-inputFocused css-1tz8xqf" and @placeholder="Искать"]')
+main_page = (By.XPATH, '//div[@class="MuiBackdrop-root MuiBackdrop-invisible MuiModal-backdrop css-esi9ax" and @aria-hidden="true"]')
 
 class SearchProjectsSite(BasePage):
     def __init__(self, browser):
--- tests/test_search_project.py
+++ tests/test_search_project.py
@@ -3,7 +3,8 @@ import allure
 import requests
 
 from selenium.webdriver.common.keys import Keys
-from page.search_projects import SearchProjectsSite, form_search, search_tag, method_search, button_search, name_project
+from page.search_projects import SearchProjectsSite, form_search, button_search, name_project, method_search, \
+    method_name, form_name_search, main_page
 
 
 @allure.feature('Запрос HTTP')
@@ -15,18 +16,11 @@ def test_projects_status_code():
 
 
 @allure.feature('Поиск проектов')
-@allure.title('Поиск по названию ')
-def test_search_projects_at_name(browser):
+@allure.title('Поиск по тэгу ')
+def test_search_projects_at_tag(browser):
     page_projects = SearchProjectsSite(browser)
     page_projects.open()
     page_projects.wait_element(form_search)
-    page_projects.wait_element(method_search)
-    method_button = page_projects.find(method_search)
-    with allure.step('Выбрать метод поиска: По тэгу'):
-        page_projects.browser.execute_script('arguments[0].click()', method_button)
-    page_projects.wait_element(search_tag)
-    tag_button = page_projects.find(search_tag)
-    page_projects.browser.execute_script('arguments[0].click()', tag_button)
     tag = page_projects.find(form_search)
     with allure.step('Ввести в поисковую строку тэг "PHP"'):
         tag.send_keys('PHP')
@@ -36,3 +30,21 @@ def test_search_projects_at_name(browser):
     page_projects.wait_element(name_project)
     assert page_projects.find(name_project), 'Поиск по тэгу отработал некорректно'
 
+@allure.feature('Поиск проектов')
+@allure.title('Поиск по названию ')
+def test_search_projects_at_name(browser):
+    page_projects = SearchProjectsSite(browser)
+    page_projects.open()
+    page_projects.wait_element(form_search)
+    page_projects.wait_element(method_search)
+    page_projects.find(method_search).click()
+    page_projects.wait_element(method_name)
+    page_projects.find(method_name).click()
+    page_projects.wait_element(main_page)
+    page_projects.find(main_page).click()
+    name = page_projects.find(form_search)
+    with allure.step('Ввести в поисковую строку название проекта "TEST"'):
+        name.send_keys('TEST')
+    name.send_keys(Keys.ENTER)
+    page_projects.wait_element(name_project)
+    assert page_projects.find(name_project), 'Поиск по названию не нашел элемент запроса'
