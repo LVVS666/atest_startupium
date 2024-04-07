d27b5c6c897dae7af2007b2d39c375ea57c68681
Василий Логинов <97108615+LVVS666@users.noreply.github.com>
2024-04-07 19:58:33 +0700

    api_test

--- db_test/tests/test_db.py
+++
@@ -1,8 +0,0 @@
-
-def test_table(db):
-    '''Создание нового навыка в базе данных, проверка его наличия'''
-    db.execute("INSERT INTO skills (title) VALUES ('Linux command line')")
-    db.connection.commit()
-    db.execute("SELECT * FROM skills")
-    result = db.fetchall()
-    assert result, 'Ошибка: Результат запроса пуст'
